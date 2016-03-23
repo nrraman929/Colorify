1977c7ab4d630f6e15051496bea4361e8f90e6ea
Alexander <williamamills@utexas.edu>
2016-03-23 15:41:08 -0500

    Added database and imageview activity

--- Colorify/app/src/main/java/williamamills/colorify/MainActivity.java
+++ Colorify/app/src/main/java/williamamills/colorify/MainActivity.java
@@ -1,16 +1,27 @@
 package williamamills.colorify;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+    Button mainButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainButton = (Button) findViewById(R.id.main_enter);
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), ImageViewActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
